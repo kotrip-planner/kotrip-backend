9f955865162c4f4d7ed10a3aa49bdee2bb83f4cf
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-02-26 14:31:26 +0900

    chore: 프로젝트 세팅

---
+++ src/main/java/com/example/kotrip/KotripApplication.java
@@ -0,0 +1,13 @@
+package com.example.kotrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KotripApplication.class, args);
+    }
+
+}
