fddccfd4ea3a7ea9f55cde43aa1e8fb056bca2e8
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-03-12 14:41:47 +0900

    feat: jpa와 AWS RDS 연동

--- src/main/java/com/example/kotrip/service/road/RoadService.java
+++ src/main/java/com/example/kotrip/service/road/RoadService.java
@@ -63,6 +63,13 @@ public class RoadService {
         long[][] w = new long[N][N];
         int[][] dp = new int[N][N];
 
+        //   0 1 2 3 4
+        // 0
+        // 1
+        // 2
+        // 3
+        // 4
+
         for (int i = 0; i < nodes.size(); i++) {
             Node startNode = nodes.get(i);
             long startLatitude = startNode.getLatitude();
