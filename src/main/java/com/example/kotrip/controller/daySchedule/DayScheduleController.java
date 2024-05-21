230c92799f28708c77427ec1744be1c4733685ea
wch-os <wch1224.s@gmail.com>
2024-05-21 20:08:45 +0900

    feat. #26 - DayScheduleController @RestController 적용 @Controller 404 에러 → @RestController

--- src/main/java/com/example/kotrip/controller/daySchedule/DayScheduleController.java
+++ src/main/java/com/example/kotrip/controller/daySchedule/DayScheduleController.java
@@ -6,12 +6,12 @@ import com.example.kotrip.dto.schedule.response.ScheduleResponseDto;
 import com.example.kotrip.service.schedule_day.DayTripService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/day")
 public class DayScheduleController {
