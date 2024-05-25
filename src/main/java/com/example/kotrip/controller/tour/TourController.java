93c22523c828a2c141e781c0972a905b1fa7860c
psangyun5108@gmail.com <psangyun5108@gmail.com>
2024-05-25 23:54:28 +0900

    city 불러오기 원상복구

--- src/main/java/com/example/kotrip/controller/tour/TourController.java
+++ src/main/java/com/example/kotrip/controller/tour/TourController.java
@@ -6,7 +6,6 @@ import com.example.kotrip.dto.tour.response.TourPageResponseDto;
 import com.example.kotrip.repository.tour.TourRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +16,9 @@ public class TourController {
     private final TourRepository tourRepository;
 
     @GetMapping("/tour")
-    public ApiResponse<TourPageResponseDto> tours(@RequestParam int cityId, @RequestParam int page) {
-        List<TourInfoDto> tourInfoDtoList = tourRepository.findTourInfoByCityId(cityId, PageRequest.of(page, 20));
-        TourPageResponseDto tourPageResponseDto = new TourPageResponseDto(page,tourInfoDtoList);
+    public ApiResponse<TourPageResponseDto> tours(@RequestParam int cityId) {
+        List<TourInfoDto> tourInfoDtoList = tourRepository.findTourInfoByCityId(cityId);
+        TourPageResponseDto tourPageResponseDto = new TourPageResponseDto(tourInfoDtoList);
         return ApiResponse.ok(tourPageResponseDto);
     }
 }
--- src/main/java/com/example/kotrip/dto/tour/response/TourPageResponseDto.java
+++ src/main/java/com/example/kotrip/dto/tour/response/TourPageResponseDto.java
@@ -5,11 +5,9 @@ import lombok.Getter;
 
 @Getter
 public class TourPageResponseDto {
-    private int page;
     private List<TourInfoDto> list;
 
-    public TourPageResponseDto(int page, List<TourInfoDto> list){
-        this.page = page;
+    public TourPageResponseDto(List<TourInfoDto> list){
         this.list = list;
     }
 }
