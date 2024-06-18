89ffc42893bbd17c292a9ef9c4f1f8a4673c69ec
wch-os <wch1224.s@gmail.com>
2024-06-18 20:50:35 +0900

    (del) : GlobalExceptionHandler 주석 처리
    적절하게 예외 메시지를 보내고 있지 않아, 일단 주석 처리를 한다.

--- src/main/java/com/example/kotrip/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/kotrip/handler/GlobalExceptionHandler.java
@@ -1,22 +1,22 @@
-package com.example.kotrip.handler;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
-@Slf4j
-@ControllerAdvice
-public class GlobalExceptionHandler {
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> exception(Exception e) {
-
-        log.info("에러 터짐요");
-        log.error("{}", e.getMessage());
-
-        return ResponseEntity
-                .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("에러가 발생했습니다.");
-    }
-}
+//package com.example.kotrip.handler;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//
+//@Slf4j
+//@ControllerAdvice
+//public class GlobalExceptionHandler {
+//    @ExceptionHandler(Exception.class)
+//    public ResponseEntity<String> exception(Exception e) {
+//
+//        log.info("에러 터짐요");
+//        log.error("{}", e.getMessage());
+//
+//        return ResponseEntity
+//                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+//                .body("에러가 발생했습니다.");
+//    }
+//}
